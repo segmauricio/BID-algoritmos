8d99f9d90fcf7860f155e1fde5cfaf74aadefa0d
segmauricio <80988655+segmauricio@users.noreply.github.com>
2023-03-02 12:42:35 -0300

    Update of the loop

--- min-to-front.js
+++ min-to-front.js
@@ -2,24 +2,19 @@
 /* Dada una matriz de valores comparables, mueve el elemento más bajo al comienzo de la matriz, desplazando hacia atrás los elementos
 que estaban antes. De lo contrario, no cambies el orden de la matriz. Dado [4,2,1,3,5], cámbialo a [1,4,2,3,5] y devuélvelo.
 Como siempre, haz esto sin usar funciones integradas.*/
-
-let arr = [4,2,1,3,5]
+let arr = [4, 2, 1, 3, 5]
 var smallest = arr[0];
+var index = 0;
 
-for(var i=1; i<arr.length; i++){
-	if(arr[i] < smallest){
-		smallest = arr[i];
-	}
-}
-arr.forEach(function(item, i) {
-    if(item == smallest){
-        arr.splice(i, 1);
-        arr.unshift(item);
+for (var i = 1; i < arr.length; i++){
+    if (arr[i] <smallest) {
+        smallest = arr[i];
+        index = i;
     }
-})
-
-// Opcion 2
-// arr.unshift(arr.splice(arr.findIndex(item => item == smallest), 1)[0])
-
+}
+for (var x = index; x > 0; x--){
+    var temp = arr[x];
+    arr[x] = arr[x-1];
+    arr[x-1] = temp;
+}
 console.log(arr);
-
